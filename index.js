0c4e9d13aa04a3264d2e296f500fb90b7c577900
SATISH <satish@technotip.org>
2022-05-31 20:18:41 +0200

    removed eval()

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 "use strict";
 exports.flagNames = function (objectType, accountflag) {
   const array = [];
-  Object.keys(eval(objectType)).forEach((flag) => {
-    if ((accountflag & eval(objectType)[flag]) === eval(objectType)[flag]) {
+  Object.keys(Flags[objectType]).forEach((flag) => {
+    if ((accountflag & Flags[objectType][flag]) === Flags[objectType][flag]) {
       array.push(flag);
     }
   });
   return array;
 };
-const AccountRoot = {
-  lsfPasswordSpent: 65536,
-  lsfRequireDestTag: 131072,
-  lsfRequireAuth: 262144,
-  lsfDisallowXRP: 524288,
-  lsfDisableMaster: 1048576,
-  lsfNoFreeze: 2097152,
-  lsfGlobalFreeze: 4194304,
-  lsfDefaultRipple: 8388608,
-  lsfDepositAuth: 16777216,
+const Flags = {
+  AccountRoot: {
+    lsfPasswordSpent: 65536,
+    lsfRequireDestTag: 131072,
+    lsfRequireAuth: 262144,
+    lsfDisallowXRP: 524288,
+    lsfDisableMaster: 1048576,
+    lsfNoFreeze: 2097152,
+    lsfGlobalFreeze: 4194304,
+    lsfDefaultRipple: 8388608,
+    lsfDepositAuth: 16777216,
+  },
+  RippleState : {
+    lsfLowReserve: 65536,
+    lsfHighReserve: 131072,
+    lsfLowAuth: 262144,
+    lsfHighAuth: 524288,
+    lsfLowNoRipple: 1048576,
+    lsfHighNoRipple: 2097152,
+    lsfLowFreeze: 4194304,
+    lsfHighFreeze: 8388608,
+  }
 };
-const RippleState = {
-  lsfLowReserve: 65536,
-  lsfHighReserve: 131072,
-  lsfLowAuth: 262144,
-  lsfHighAuth: 524288,
-  lsfLowNoRipple: 1048576,
-  lsfHighNoRipple: 2097152,
-  lsfLowFreeze: 4194304,
-  lsfHighFreeze: 8388608,
-};
+
+
